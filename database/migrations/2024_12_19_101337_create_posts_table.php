81dfb713ecdde2f89c1eb69f96bc1b78ce3e4dd1
Aakriti Dhungel <aakritidhungel01@gmail.com>
2024-12-19 16:11:30 +0545

    table join using foreign key

---
+++ database/migrations/2024_12_19_101337_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->longText('description');
+            $table->integer('views');
+            $table->longText('meta-words')->default(0);
+            $table->longText('meta-description')->nullable();
+            $table->longText('status')->default('pending');
+
+            $table->timestamps();
+        });
+
+
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category-id')->constrained()->cascadeOnDelete(); //cascadeOnDelete() yo rakda category ko post ko sabai delete hunxa
+            $table->foreignId('post-id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
