f12edc521386d13cee05d91a325887a5afc29f24
Aakriti Dhungel <aakritidhungel01@gmail.com>
2024-12-23 14:52:20 +0545

    header design

--- app/Http/Controllers/Frontend/PageController.php
+++ app/Http/Controllers/Frontend/PageController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\View\Components\FrontendLayout;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home(){
+        return view('frontend.home');
+    }
 }
--- resources/views/components/frontend-layout.blade.php
+++ resources/views/components/frontend-layout.blade.php
@@ -6,12 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
+    <style>
+        .container{
+            width:86%;
+            margin: auto;
+        }
+    </style>
 </head>
 
 <body>
 
     <header>
-
+        <div class="container flex justify-between items-center">
+            <div>
+                <img class="h-[100px]" src="https://jawaaf.com/storage/01JF6CJYGS53X1S7DZ5XKFC705.png" alt="Company Logo">
+            </div>
+            <div>
+                {{nepalidate(now())}}
+                <img class="h-[20px]" src="https://jawaaf.com/frontend/images/redline.png" alt="Line">
+            </div>
+        </div>
+        <x-frontend-navbar/>
     </header>
 
     <main>
