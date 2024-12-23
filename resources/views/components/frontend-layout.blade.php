9ce7876004076c9e16b469b9f2f562e2040f2115
Aakriti Dhungel <aakritidhungel01@gmail.com>
2024-12-23 12:27:10 +0545

    flowbite installation for frontend

--- resources/views/components/frontend-layout.blade.php
+++ resources/views/components/frontend-layout.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
